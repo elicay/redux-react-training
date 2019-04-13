07a87a012d4cc7538757e049a93fdda2c3087229
errold cristian licay <errold@pop-os.localdomain>
2019-04-13 18:41:57 +0800

    basic redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import App from './components/App';
+// import App from './components/App';
 
-ReactDOM.render(<App></App>, document.getElementById('app'));
+// ReactDOM.render(<App></App>, document.getElementById('app'));
+
+import { createStore } from 'redux';
+
+const initialState = {
+  result: 1,
+  lastValues: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, state.result + action.payload]
+      }
+      break;
+
+    case 'SUBTRACT':
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, state.result - action.payload]
+      }
+      break;
+  
+    default:
+      break;
+  }
+
+  return state;
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({
+  type: 'ADD',
+  payload: 6
+})
+
+store.dispatch({
+  type: 'SUBTRACT',
+  payload: 100
+})
